e4864a88b94f9f979f8640492051a2b48b3e8f3b
Jessy <japarker0421@gmail.com>
2023-02-21 15:29:58 -0700

    got package Json up and runing

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-const PORT = 8000;
+// const PORT = 8000;
 
-const express = require("express");
-const server = express();
+// const express = require("express");
+// const server = express();
 
-const morgan = require("morgan");
-server.use(morgan("dev"));
+// const morgan = require("morgan");
+// server.use(morgan("dev"));
 
-server.use(express.json());
+// server.use(express.json());
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Main } from "./components";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { Main } from "./components";
 
-const container = document.getElementById("app");
-const root = ReactDOM.createRoot(container);
+// const container = document.getElementById("app");
+// const root = ReactDOM.createRoot(container);
 
-root.render(<Main />);
+// root.render(<Main />);
