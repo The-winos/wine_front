30660433b4d9b1fa5f8b9f979283d08eac9d1e6d
Jessy <japarker0421@gmail.com>
2023-02-23 12:31:05 -0700

    starting functions adde with first api call

--- src/components/API/index.js
+++ src/components/API/index.js
@@ -1,1 +1,16 @@
+const BASE_URL = "http://localhost:8080/api";
 
+export async function authUser(token) {
+  try {
+    const response = await fetch(`${BASE_URL}/users/me`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
