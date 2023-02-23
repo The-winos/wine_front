6bbf6914d65ee312e7a63777e10959728ccda731
Jessy <japarker0421@gmail.com>
2023-02-23 13:15:54 -0700

    Basic navbar set up with links working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
-const Navbar = () => {
-  return (
+const Navbar = (props) => {
+  const setLoggedIn = props.setLoggedIn;
+  const loggedIn = props.loggedIn;
+  const user = props.user;
+  const setUser = props.setUser;
+  const navigate = useNavigate();
+  return (<>
     <div id="navbar">
-      <h2>I am HERE Navbar</h2>
+      <div className="links">
+      <div id="username">
+        {loggedIn ? <h5>Welcome, {`${user.username}`}</h5> : <h5>Please log in</h5>}
+      </div>
+      <NavLink className="linkBar" to="/">Home</NavLink>
+      {loggedIn ? (
+        <>
+        <NavLink to={"/"} className="linkBar" onClick={()=>{
+          navigate("/login");
+          localStorage.removeItem("token");
+          setLoggedIn(false);
+          setUser(null);
+        }}>LogOut</NavLink>
+        <NavLink className="linkBar" to="/profile">
+          <span id="profile-hover" data-hover="my account">
+            </span>
+            Profile
+        </NavLink>
+
+        {loggedIn && user.merchant || user.admin ?(
+          <NavLink className="linkBar" to= "/admin">
+            <span id="admin-hover" data-hover="Admin"></span>ADMIN
+          </NavLink>
+        ): null}
+        </>
+      ):(
+        <NavLink className= "linkBar" to="/login">Login</NavLink>
+      )}
+      <NavLink className="linkBar" to="/winefeed">The Tasting Room</NavLink>
+      <NavLink className="linkBar" to="/followers">Friend's wine</NavLink>
+      </div>
     </div>
+    </>
   );
 };
 
