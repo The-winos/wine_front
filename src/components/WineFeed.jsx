a808fbdf247844389bbdb1a3a66b6ad9b070c5b0
Jessy <japarker0421@gmail.com>
2023-02-27 21:11:58 -0700

    working on the review feed

--- src/components/WineFeed.jsx
+++ src/components/WineFeed.jsx
@@ -1,9 +1,10 @@
 import React, {useEffect} from "react";
 import { getAllReviews, getAllWine } from "./API";
+import ReviewDetails from "./ReviewDetails";
 import WineDetails from "./WineDetails";
 
 
-const WineFeed = ({allWine, setAllWine, setWineInfo, wineInfo, setAllReviews, allReviews}) => {
+const WineFeed = ({allWine, setAllWine, setWineInfo, wineInfo, setAllReviews, allReviews, setReviewInfo, reviewInfo}) => {
 
   useEffect(()=>{
   async function fetchAllReview(){
@@ -14,7 +15,24 @@ const WineFeed = ({allWine, setAllWine, setWineInfo, wineInfo, setAllReviews, al
   fetchAllReview();
 }, []);
 
-  return <div id="winFeed"></div>;
+  return (
+  <div id="winFeed">
+<h2 id="tastingRoom">Welcome to the Tasting Room</h2>
+<h3 id="tastingRoomInfo">See all the wine people have reviewed</h3>
+<div id="reviews" className="review">
+  {allReviews && allReviews.length ? allReviews.map((review)=>{
+    return(
+      <div key={`allreviews-${review.id}`}>
+        <ReviewDetails
+        review={review}
+        setReviewInfo={setReviewInfo}
+        />
+      </div>
+    );
+  }):null}
+</div>
+
+  </div>);
 };
 
 export default WineFeed;
