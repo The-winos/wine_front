82ff5a6aa2e2752933dae1cc9b311d4e184b9375
Jessy <japarker0421@gmail.com>
2023-03-12 19:50:26 -0400

    got the handlefollow to show correct info, had to change the backend call

--- src/components/FollowButton.jsx
+++ src/components/FollowButton.jsx
@@ -6,9 +6,7 @@ const FollowButton = ({review, reviewUser, reviewWine, user}) => {
   const [isFollowing, setIsFollowing] = useState(false);
 
  async function handleFollow() {
-    // Check if the current user is already following the wine friend
     const isAlreadyFollowing = await getFollowersById(user.id)
-    console.log(user.id, "is this a 2?")
     console.log(isAlreadyFollowing, "whats this look like??")
     if (isAlreadyFollowing) {
       setIsFollowing(true);
