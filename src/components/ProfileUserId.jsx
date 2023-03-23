3d273704915f96a886cd66f555b11c749da0e02a
Jessy <japarker0421@gmail.com>
2023-03-23 13:11:25 -0600

    link working in winefeed to user profile and non logged in user profile working

--- src/components/ProfileUserId.jsx
+++ src/components/ProfileUserId.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { getReviewByUser } from "./API";
+import { getReviewByUser, getUserById } from "./API";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import UserReviewDetails from "./UserReviewDetails";
+import UserIdReviewDetails from "./UserIdReviewDetails";
 
 const ProfileUserId = ({ user }) => {
   const { id } = useParams();
   const [userReviews, setUserReviews] = useState([]);
+  const [userProfile, setUserProfile]= useState({})
 
   useEffect(() => {
     const fetchUserReviews = async () => {
       try {
         const reviews = await getReviewByUser(id);
         setUserReviews(reviews);
-        console.log(reviews, "user reviews");
       } catch (error) {
         console.error(error);
       }
@@ -21,13 +22,20 @@ const ProfileUserId = ({ user }) => {
     fetchUserReviews();
   }, []);
 
+  useEffect(()=>{
+    async function fetchGetUserById(){
+      const theUser= await getUserById(id)
+      console.log(theUser)
+      setUserProfile(theUser);
+}
+fetchGetUserById();
+  }, []);
+
   return (
     <div className="profile-container">
       <div id="profile-main">
-        {console.log(user, "is this working?")}
-        {console.log(user.id, "is this id?")}
         <img
-          src={user.avatar}
+          src={userProfile.avatar}
           alt="avatar image"
           className="img-fluid"
           style={{
@@ -38,42 +46,31 @@ const ProfileUserId = ({ user }) => {
           }}
         />
         <div>
-          <Link to={"/accountsettings"}>
-            <button type="accountsettings" className="buttons">
-              Account Settings
-            </button>
-          </Link>
           <Link to={"/favorite"}>
             <button type="favorite" className="buttons">
               Favorites
             </button>
           </Link>
         </div>
-        <h2 className="profile-username">{user.name}</h2>
+        <h2 className="profile-username">{userProfile.name}</h2>
+        <h2 className="profile-username">I follow {userProfile.following_count} people</h2>
+        <h2 className="profile-username">{userProfile.follower_count} people follow me!</h2>
       </div>
 
       <div>
-        {user && user.admin ? (
-          <>
-            <Link to={"/users"}>
-              <button type="all users" className="buttons">
-                All Users
-              </button>
-            </Link>
-          </>
-        ) : null}
-      </div>
+        <h3 className="profile-review-list">{userProfile.username}'s Reviews</h3>
 
-      <div>
-        <h3 className="profile-review-list">Your Reviews</h3>
-
-        <div key={`userReview-${userReviews.id}`}>
-          <UserReviewDetails
-            user={user}
+        {userReviews && userReviews.length ? (
+          userReviews.map((userReviews) => {
+            return (
+              <div key={`useridReview-${userReviews.id}`}>
+          <UserIdReviewDetails
             userReviews={userReviews}
             setUserReviews={setUserReviews}
+            userProfile={userProfile}
           />
         </div>
+            )})): null}
       </div>
     </div>
   );
--- src/components/ReviewDetails.jsx
+++ src/components/ReviewDetails.jsx
@@ -62,7 +62,7 @@ fetchGetUserById();
 <h4 className="review-title">{review.name}</h4>
 <div className="d-flex align-items-center">
   <small className="text-muted">By:
-    <a href={`/profile/${reviewUser.id}`}>{reviewUser.username}</a>
+    <a href={`/profileuserid/${reviewUser.id}`}>{reviewUser.username}</a>
   </small>
   <div className="ml-3">
     <FollowButton
