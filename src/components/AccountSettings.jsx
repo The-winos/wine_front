02d90838333abc50f685c3b573f57d361fa8191c
Jenniffer Melchiade <jdenisemelch@gmail.com>
2023-04-04 18:49:42 -0400

    need to work on account settings

--- src/components/AccountSettings.jsx
+++ src/components/AccountSettings.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { authUser } from "./API";
 import EditAccount from "./EditAccount";
 
@@ -19,109 +20,14 @@ const AccountSettings = ({ users }) => {
           <EditAccount users={users} />
         ) : null}
       </>
-      <button className="admin-buttons" id={users} onClick={handleChooseEdit}>
-        Update
-      </button>
+      <div></div>
+      <Link to={"/editaccount"}>
+        {" "}
+        <button className="admin-buttons" id={users} onClick={handleChooseEdit}>
+          Update
+        </button>
+      </Link>
       <div>
-        <form className="admin-form">
-          <div id="text-fields">Account Settings</div>
-          <input
-            placeholder="name"
-            className="first-name"
-            type="text"
-            onChange={(event) => {
-              setName(event.target.value);
-            }}
-          ></input>
-          <input
-            placeholder="name"
-            className="last-name"
-            type="text"
-            onChange={(event) => {
-              setLastName(event.target.value);
-            }}
-          ></input>
-
-          <div id="text-fields">Location</div>
-          <select
-            placeholder="location"
-            className="location"
-            type="text"
-            value="location"
-            onChange={(event) => {
-              setLocation(event.target.value);
-            }}
-            required
-          >
-            <option value="">Select State</option>
-            <option value="AL">Alabama</option>
-            <option value="AK">Alaska</option>
-            <option value="AZ">Arizona</option>
-            <option value="AR">Arkansas</option>
-            <option value="CA">California</option>
-            <option value="CO">Colorado</option>
-            <option value="CT">Connecticut</option>
-            <option value="DE">Delaware</option>
-            <option value="DC">District of Columbia</option>
-            <option value="FL">Florida</option>
-            <option value="GA">Georgia</option>
-            <option value="HI">Hawaii</option>
-            <option value="ID">Idaho</option>
-            <option value="IL">Illinois</option>
-            <option value="IN">Indiana</option>
-            <option value="IA">Iowa</option>
-            <option value="KS">Kansas</option>
-            <option value="KY">Kentucky</option>
-            <option value="LA">Louisiana</option>
-            <option value="ME">Maine</option>
-            <option value="MD">Maryland</option>
-            <option value="MA">Massachusetts</option>
-            <option value="MI">Michigan</option>
-            <option value="MN">Minnesota</option>
-            <option value="MS">Mississippi</option>
-            <option value="MO">Missouri</option>
-            <option value="MT">Montana</option>
-            <option value="NE">Nebraska</option>
-            <option value="NV">Nevada</option>
-            <option value="NH">New Hampshire</option>
-            <option value="NJ">New Jersey</option>
-            <option value="NM">New Mexico</option>
-            <option value="NY">New York</option>
-            <option value="NC">North Carolina</option>
-            <option value="ND">North Dakota</option>
-            <option value="OH">Ohio</option>
-            <option value="OK">Oklahoma</option>
-            <option value="OR">Oregon</option>
-            <option value="PA">Pennsylvania</option>
-            <option value="RI">Rhode Island</option>
-            <option value="SC">South Carolina</option>
-            <option value="SD">South Dakota</option>
-            <option value="TN">Tennessee</option>
-            <option value="TX">Texas</option>
-            <option value="UT">Utah</option>
-            <option value="VT">Vermont</option>
-            <option value="VA">Virginia</option>
-            <option value="WA">Washington</option>
-            <option value="WV">West Virginia</option>
-            <option value="WI">Wisconsin</option>
-            <option value="WY">Wyoming</option>
-          </select>
-
-          <div id="bio-text-field">
-            Summary
-            <textarea
-              placeholder="bio"
-              className="bio"
-              type="text"
-              onChange={(event) => {
-                setBio(event.target.value);
-              }}
-            ></textarea>
-          </div>
-          <button id="admin-submit" onClick={handleUpdateAdmin} type="submit">
-            Submit
-          </button>
-        </form>
         <button
           id="admin-cancel-edit"
           onClick={() => {
