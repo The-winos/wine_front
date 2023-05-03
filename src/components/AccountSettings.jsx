11ebaabac7c9d2a20cff3df94493573e94894621
Jenniffer Melchiade <jdenisemelch@gmail.com>
2023-05-03 18:56:52 -0400

    Revising form, need to pass ID

--- src/components/AccountSettings.jsx
+++ src/components/AccountSettings.jsx
@@ -3,23 +3,40 @@ import { Link } from "react-router-dom";
 import { updateUser } from "./API";
 
 const AccountSettings = ({ user }) => {
-  const [userInfo, setUserInfo] = useState(false);
-  const [bio, setBio] = useState("");
-  const [name, setName] = useState("");
-  const [location, setLocation] = useState("");
+  const [username, setUsername] = useState(user.username || "");
+  const [state, setState] = useState(user.state || "");
+  const [avatar, setAvatar] = useState(user.avatar || "");
+  const [email, setEmail] = useState(user.email || "");
+  const [birthday, setBirthday] = useState(user.birthday || "");
+  const [bio, setBio] = useState(user.bio || "");
+  const [password, setPassword] = useState(user.password || "");
+  const [update, setUpdate] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    if (
+      username === user.username &&
+      state === user.state &&
+      avatar === user.avatar &&
+      email === user.email &&
+      birthday === user.birthday &&
+      bio === user.bio &&
+      password === user.password
+    ) {
+      // no changes made, do not update user
+      return;
+    }
     try {
       const updatedUser = {
-        name,
-        location,
+        username,
+        password,
+        state,
+        avatar,
+        email,
+        birthday,
         bio,
       };
-      console.log(updatedUser);
-
-      const response = await updateUser(user.username, updatedUser);
-      setUserInfo(response);
+      const response = await updateUser(user.id, updatedUser);
       setUpdate(true);
     } catch (error) {
       console.error(error);
@@ -43,96 +60,109 @@ const AccountSettings = ({ user }) => {
       </div>
       {console.log(user, "this is user")}
       <form onSubmit={handleSubmit} className="admin-form">
-        <>
-          <h6 id="text-fields">Name:</h6>
-          <input
-            placeholder={user.name}
-            className="first-name"
-            type="text"
-            value={name}
-            onChange={(event) => {
-              setName(event.target.value);
-            }}
-          />
-          <h6 id="text-fields">Location:</h6>
-          <select
-            placeholder="location"
-            className="location"
-            type="text"
-            value={location}
-            onChange={(event) => {
-              setLocation(event.target.value);
-            }}
-          >
-            <option value="">{user.state}</option>
-            <option value="AL">Alabama</option>
-            <option value="AK">Alaska</option>
-            <option value="AZ">Arizona</option>
-            <option value="AR">Arkansas</option>
-            <option value="CA">California</option>
-            <option value="CO">Colorado</option>
-            <option value="CT">Connecticut</option>
-            <option value="DE">Delaware</option>
-            <option value="DC">District of Columbia</option>
-            <option value="FL">Florida</option>
-            <option value="GA">Georgia</option>
-            <option value="HI">Hawaii</option>
-            <option value="ID">Idaho</option>
-            <option value="IL">Illinois</option>
-            <option value="IN">Indiana</option>
-            <option value="IA">Iowa</option>
-            <option value="KS">Kansas</option>
-            <option value="KY">Kentucky</option>
-            <option value="LA">Louisiana</option>
-            <option value="ME">Maine</option>
-            <option value="MD">Maryland</option>
-            <option value="MA">Massachusetts</option>
-            <option value="MI">Michigan</option>
-            <option value="MN">Minnesota</option>
-            <option value="MS">Mississippi</option>
-            <option value="MO">Missouri</option>
-            <option value="MT">Montana</option>
-            <option value="NE">Nebraska</option>
-            <option value="NV">Nevada</option>
-            <option value="NH">New Hampshire</option>
-            <option value="NJ">New Jersey</option>
-            <option value="NM">New Mexico</option>
-            <option value="NY">New York</option>
-            <option value="NC">North Carolina</option>
-            <option value="ND">North Dakota</option>
-            <option value="OH">Ohio</option>
-            <option value="OK">Oklahoma</option>
-            <option value="OR">Oregon</option>
-            <option value="PA">Pennsylvania</option>
-            <option value="RI">Rhode Island</option>
-            <option value="SC">South Carolina</option>
-            <option value="SD">South Dakota</option>
-            <option value="TN">Tennessee</option>
-            <option value="TX">Texas</option>
-            <option value="UT">Utah</option>
-            <option value="VT">Vermont</option>
-            <option value="VA">Virginia</option>
-            <option value="WA">Washington</option>
-            <option value="WV">West Virginia</option>
-            <option value="WI">Wisconsin</option>
-            <option value="WY">Wyoming</option>
-          </select>
-          <div></div>
-          <h6>My Bio:</h6>
-          <textarea
-            placeholder="bio"
-            className="bio"
-            type="text"
-            onChange={(event) => {
-              setBio(event.target.value);
-            }}
-            value={bio}
-          />
+        {update ? (
+          <>
+            <h6 id="text-fields">Username:</h6>
+            <input
+              placeholder={user.username}
+              className="first-name"
+              type="text"
+              value={username}
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
+            />
+            <h6 id="text-fields">Password:</h6>
+            <input
+              placeholder={user.password}
+              className="first-name"
+              type="text"
+              value={password}
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+            <h6 id="text-fields">Location:</h6>
+            <div>{user.state}</div>
+            <select
+              placeholder="location"
+              className="location"
+              type="text"
+              value={state}
+              onChange={(event) => {
+                setState(event.target.value);
+              }}
+            >
+              <option value=""> </option>
+              <option value="AL">Alabama</option>
+              <option value="AK">Alaska</option>
+              <option value="AZ">Arizona</option>
+              <option value="AR">Arkansas</option>
+              <option value="CA">California</option>
+              <option value="CO">Colorado</option>
+              <option value="CT">Connecticut</option>
+              <option value="DE">Delaware</option>
+              <option value="DC">District of Columbia</option>
+              <option value="FL">Florida</option>
+              <option value="GA">Georgia</option>
+              <option value="HI">Hawaii</option>
+              <option value="ID">Idaho</option>
+              <option value="IL">Illinois</option>
+              <option value="IN">Indiana</option>
+              <option value="IA">Iowa</option>
+              <option value="KS">Kansas</option>
+              <option value="KY">Kentucky</option>
+              <option value="LA">Louisiana</option>
+              <option value="ME">Maine</option>
+              <option value="MD">Maryland</option>
+              <option value="MA">Massachusetts</option>
+              <option value="MI">Michigan</option>
+              <option value="MN">Minnesota</option>
+              <option value="MS">Mississippi</option>
+              <option value="MO">Missouri</option>
+              <option value="MT">Montana</option>
+              <option value="NE">Nebraska</option>
+              <option value="NV">Nevada</option>
+              <option value="NH">New Hampshire</option>
+              <option value="NJ">New Jersey</option>
+              <option value="NM">New Mexico</option>
+              <option value="NY">New York</option>
+              <option value="NC">North Carolina</option>
+              <option value="ND">North Dakota</option>
+              <option value="OH">Ohio</option>
+              <option value="OK">Oklahoma</option>
+              <option value="OR">Oregon</option>
+              <option value="PA">Pennsylvania</option>
+              <option value="RI">Rhode Island</option>
+              <option value="SC">South Carolina</option>
+              <option value="SD">South Dakota</option>
+              <option value="TN">Tennessee</option>
+              <option value="TX">Texas</option>
+              <option value="UT">Utah</option>
+              <option value="VT">Vermont</option>
+              <option value="VA">Virginia</option>
+              <option value="WA">Washington</option>
+              <option value="WV">West Virginia</option>
+              <option value="WI">Wisconsin</option>
+              <option value="WY">Wyoming</option>
+            </select>
+            <div></div>
+            <h6>My Bio:</h6>
+            <textarea
+              placeholder="bio"
+              className="bio"
+              type="text"
+              onChange={(event) => {
+                setBio(event.target.value);
+              }}
+              value={bio}
+            />
 
-          <button type="submit" className="buttons">
-            Save Changes
-          </button>
-        </>
+            <button type="submit" className="buttons">
+              Save Changes
+            </button>
+          </>
+        ) : null}
       </form>
 
       <Link to={"/profile"}>
