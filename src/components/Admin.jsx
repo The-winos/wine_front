f4f95e52f74345d447b6886b4a419eb239b2df1a
Jessy <japarker0421@gmail.com>
2023-05-16 15:53:43 -0600

    added username and changed look, not opperational yet

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -6,18 +6,18 @@ import { getAllUsers, getUserById } from "./API";
 const Admin = ({ user }) => {
   const [allUsers, setAllUser] = useState([]);
   const [userButton, setUserButton] = useState(false);
-  const [updateUser, setUpdateUser]= useState(false);
-  const [updatingUser, setUpdatingUser]=useState({})
-  const [name, setName]=useState(updatingUser.name)
-  const[birthday, setBirthday]=useState(updatingUser.birthday)
-  const[state, setState]= useState(updatingUser.state)
+  const [updateUser, setUpdateUser] = useState(false);
+  const [updatingUser, setUpdatingUser] = useState({});
+  const [username, setUsername] = useState(updatingUser.username);
+  const [name, setName] = useState(updatingUser.name);
+  const [birthday, setBirthday] = useState(updatingUser.birthday);
+  const [state, setState] = useState(updatingUser.state);
   const [bio, setBio] = useState(updatingUser.bio);
   const [password, setPassword] = useState(updatingUser.password);
   const [newPassword, setNewPassword] = useState("");
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [update, setUpdate] = useState(true);
   const [formattedBirthday, setFormattedBirthday] = useState("");
-
 
   useEffect(() => {
     async function fetchAllUsers() {
@@ -49,79 +49,168 @@ const Admin = ({ user }) => {
     setFormattedBirthday(formattedDate);
   }, [birthday]);
 
-
-  async function handleUserClick (userId){
-    setUpdateUser(true)
-    const userToUpdate=await getUserById(userId)
-    setUpdatingUser(userToUpdate)
-}
-
-async function handleSubmit(event) {
-  event.preventDefault();
-  if (
-    name === user.name &&
-    state === user.state &&
-    avatar === user.avatar &&
-    email === user.email &&
-    birthday === user.birthday &&
-    bio === user.bio &&
-    password === user.password
-  ) {
-    // no changes made, do not update user
-    return;
+  async function handleUserClick(userId) {
+    setUpdateUser(true);
+    const userToUpdate = await getUserById(userId);
+    setUpdatingUser(userToUpdate);
   }
 
-  try {
-    const updateInfo = await updateUser(
-      user.id,
-      user.username,
-      password,
-      name,
-      state,
-      user.role,
-      email,
-      formattedBirthday,
-      user.follower_count,
-      user.following_count
-    );
-  } catch (error) {
-    console.error(error);
-    setUpdate(true);
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (
+      username === updatingUser.username &&
+      name === updatingUser.name &&
+      state === updatingUser.state &&
+      avatar === updatingUser.avatar &&
+      email === updatingUser.email &&
+      birthday === updatingUser.birthday &&
+      bio === updatingUser.bio &&
+      password === updatingUser.password
+    ) {
+      return;
+    }
+
+    try {
+      const updateInfo = await updateUser(
+        user.id,
+        username,
+        password,
+        name,
+        state,
+        user.role,
+        email,
+        formattedBirthday,
+        user.follower_count,
+        user.following_count
+      );
+    } catch (error) {
+      console.error(error);
+      setUpdate(true);
+    }
   }
-}
 
   return (
     <div id="admin">
+      <h2 className="d-flex justify-content-center ">
+        Welcome to the Admin Portal
+      </h2>
       <>
-      {updateUser ? (
-        <>
-        <h3>You are updating {updatingUser.username}</h3>
+        {updateUser ? (
+          <>
+            <h3 className="d-flex justify-content-center">
+              You are updating {updatingUser.username}
+            </h3>
 
-            <h3>{updatingUser.name}</h3>
-            <h6>Update Name</h6>
-            <input
-              placeholder="Enter your name"
-              className="first-name"
-              type="text"
-              value={name}
-              onChange={(event) => {
-                setName(event.target.value);
-              }}
-            />
-            <div></div>
-            <h3>Birthday {formattedBirthday}</h3>
-            <h6>Update Birthday</h6>
+            <div className="row">
+              <div className="col">
+                <h5>{updatingUser.username}</h5>
+                <h6>Update Username</h6>
+                <input
+                  placeholder="update username"
+                  className="username"
+                  type="text"
+                  value={username}
+                  onChange={(event) => {
+                    setUsername(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="col">
+                <h5>{updatingUser.name}</h5>
+                <h6>Update Name</h6>
+                <input
+                  placeholder="Enter updated name"
+                  className="first-name"
+                  type="text"
+                  value={name}
+                  onChange={(event) => {
+                    setName(event.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <h5>Birthday {formattedBirthday}</h5>
+                <h6>Update Birthday</h6>
 
-            <input
-              placeholder={formattedBirthday}
-              className="first-name"
-              type="text"
-              value={birthday}
-              onChange={(event) => {
-                setBirthday(event.target.value);
-              }}
-            />
-            {console.log(formattedBirthday)}
+                <input
+                  placeholder={formattedBirthday}
+                  className="first-name"
+                  type="text"
+                  value={birthday}
+                  onChange={(event) => {
+                    setBirthday(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="col">
+                {console.log(formattedBirthday)}
+                <h6>State {updatingUser.state}</h6>
+                <select
+                  placeholder="location"
+                  className="location"
+                  type="text"
+                  value={state}
+                  onChange={(event) => {
+                    setState(event.target.value);
+                  }}
+                >
+                  <option value=""> </option>
+                  <option value="AL">Alabama</option>
+                  <option value="AK">Alaska</option>
+                  <option value="AZ">Arizona</option>
+                  <option value="AR">Arkansas</option>
+                  <option value="CA">California</option>
+                  <option value="CO">Colorado</option>
+                  <option value="CT">Connecticut</option>
+                  <option value="DE">Delaware</option>
+                  <option value="DC">District of Columbia</option>
+                  <option value="FL">Florida</option>
+                  <option value="GA">Georgia</option>
+                  <option value="HI">Hawaii</option>
+                  <option value="ID">Idaho</option>
+                  <option value="IL">Illinois</option>
+                  <option value="IN">Indiana</option>
+                  <option value="IA">Iowa</option>
+                  <option value="KS">Kansas</option>
+                  <option value="KY">Kentucky</option>
+                  <option value="LA">Louisiana</option>
+                  <option value="ME">Maine</option>
+                  <option value="MD">Maryland</option>
+                  <option value="MA">Massachusetts</option>
+                  <option value="MI">Michigan</option>
+                  <option value="MN">Minnesota</option>
+                  <option value="MS">Mississippi</option>
+                  <option value="MO">Missouri</option>
+                  <option value="MT">Montana</option>
+                  <option value="NE">Nebraska</option>
+                  <option value="NV">Nevada</option>
+                  <option value="NH">New Hampshire</option>
+                  <option value="NJ">New Jersey</option>
+                  <option value="NM">New Mexico</option>
+                  <option value="NY">New York</option>
+                  <option value="NC">North Carolina</option>
+                  <option value="ND">North Dakota</option>
+                  <option value="OH">Ohio</option>
+                  <option value="OK">Oklahoma</option>
+                  <option value="OR">Oregon</option>
+                  <option value="PA">Pennsylvania</option>
+                  <option value="RI">Rhode Island</option>
+                  <option value="SC">South Carolina</option>
+                  <option value="SD">South Dakota</option>
+                  <option value="TN">Tennessee</option>
+                  <option value="TX">Texas</option>
+                  <option value="UT">Utah</option>
+                  <option value="VT">Vermont</option>
+                  <option value="VA">Virginia</option>
+                  <option value="WA">Washington</option>
+                  <option value="WV">West Virginia</option>
+                  <option value="WI">Wisconsin</option>
+                  <option value="WY">Wyoming</option>
+                </select>
+              </div>
+            </div>
 
             <h6 id="text-fields">Password:</h6>
             <div className="container">
@@ -157,71 +246,6 @@ async function handleSubmit(event) {
                 </div>
               </div>
             </div>
-
-            <h6>State {updatingUser.state}</h6>
-            <select
-              placeholder="location"
-              className="location"
-              type="text"
-              value={state}
-              onChange={(event) => {
-                setState(event.target.value);
-              }}
-            >
-              <option value=""> </option>
-              <option value="AL">Alabama</option>
-              <option value="AK">Alaska</option>
-              <option value="AZ">Arizona</option>
-              <option value="AR">Arkansas</option>
-              <option value="CA">California</option>
-              <option value="CO">Colorado</option>
-              <option value="CT">Connecticut</option>
-              <option value="DE">Delaware</option>
-              <option value="DC">District of Columbia</option>
-              <option value="FL">Florida</option>
-              <option value="GA">Georgia</option>
-              <option value="HI">Hawaii</option>
-              <option value="ID">Idaho</option>
-              <option value="IL">Illinois</option>
-              <option value="IN">Indiana</option>
-              <option value="IA">Iowa</option>
-              <option value="KS">Kansas</option>
-              <option value="KY">Kentucky</option>
-              <option value="LA">Louisiana</option>
-              <option value="ME">Maine</option>
-              <option value="MD">Maryland</option>
-              <option value="MA">Massachusetts</option>
-              <option value="MI">Michigan</option>
-              <option value="MN">Minnesota</option>
-              <option value="MS">Mississippi</option>
-              <option value="MO">Missouri</option>
-              <option value="MT">Montana</option>
-              <option value="NE">Nebraska</option>
-              <option value="NV">Nevada</option>
-              <option value="NH">New Hampshire</option>
-              <option value="NJ">New Jersey</option>
-              <option value="NM">New Mexico</option>
-              <option value="NY">New York</option>
-              <option value="NC">North Carolina</option>
-              <option value="ND">North Dakota</option>
-              <option value="OH">Ohio</option>
-              <option value="OK">Oklahoma</option>
-              <option value="OR">Oregon</option>
-              <option value="PA">Pennsylvania</option>
-              <option value="RI">Rhode Island</option>
-              <option value="SC">South Carolina</option>
-              <option value="SD">South Dakota</option>
-              <option value="TN">Tennessee</option>
-              <option value="TX">Texas</option>
-              <option value="UT">Utah</option>
-              <option value="VT">Vermont</option>
-              <option value="VA">Virginia</option>
-              <option value="WA">Washington</option>
-              <option value="WV">West Virginia</option>
-              <option value="WI">Wisconsin</option>
-              <option value="WY">Wyoming</option>
-            </select>
-            <div></div>
             <h6>My Bio:</h6>
             <textarea
               placeholder="bio"
@@ -240,17 +264,14 @@ async function handleSubmit(event) {
             >
               Save Changes
             </button>
-
-
-
-        </>
-      ): null}
+          </>
+        ) : null}
         {userButton ? (
           <>
             <button
               onClick={() => {
                 setUserButton(false);
-                setUpdateUser(false)
+                setUpdateUser(false);
               }}
               className="btn btn-primary pb-2"
             >
@@ -258,46 +279,43 @@ async function handleSubmit(event) {
             </button>
             {allUsers && allUsers.length ? (
               <>
-            <div className="text-center py-2">
-  <span className="badge bg-danger mx-2 rounded-circle pr-3">
-    &nbsp;
-  </span>
-  Admin
-  <span className="badge bg-primary mx-2 rounded-circle">
-    &nbsp;
-  </span>
-  Merchant
-  <span className="badge bg-secondary mx-2 rounded-circle">
-    &nbsp;
-  </span>
-  User
-</div>
+                <div className="text-center py-2">
+                  <span className="badge bg-danger mx-2 rounded-circle pr-3">
+                    &nbsp;
+                  </span>
+                  Admin
+                  <span className="badge bg-primary mx-2 rounded-circle">
+                    &nbsp;
+                  </span>
+                  Merchant
+                  <span className="badge bg-secondary mx-2 rounded-circle">
+                    &nbsp;
+                  </span>
+                  User
+                </div>
 
-
-{allUsers
-  .sort((a, b) => a.username.localeCompare(b.username))
-  .map((user) => {
-    return (
-      <div key={`userlist-${user.id}`}>
-        <h5
-          style={{
-            color:
-              user.role === "admin"
-                ? "red"
-                : user.role === "merchant"
-                ? "blue"
-                : "black",
-            cursor: "pointer",
-          }}
-          onClick={() => handleUserClick(user.id)}
-        >
-          {user.username}
-        </h5>
-      </div>
-    );
-  })
-}
-
+                {allUsers
+                  .sort((a, b) => a.username.localeCompare(b.username))
+                  .map((user) => {
+                    return (
+                      <div key={`userlist-${user.id}`}>
+                        <h5
+                          style={{
+                            color:
+                              user.role === "admin"
+                                ? "red"
+                                : user.role === "merchant"
+                                ? "blue"
+                                : "black",
+                            cursor: "pointer",
+                          }}
+                          onClick={() => handleUserClick(user.id)}
+                        >
+                          {user.username}
+                        </h5>
+                      </div>
+                    );
+                  })}
               </>
             ) : (
               <h2>No users found</h2>
