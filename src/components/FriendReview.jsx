ece71ac06032622de009106af67ada85aa40a3ab
Jessy <japarker0421@gmail.com>
2023-07-27 14:53:22 -0600

    added favorite and saved buttons to happy hours with auto updating

--- src/components/FriendReview.jsx
+++ src/components/FriendReview.jsx
@@ -1,17 +1,22 @@
 import React, {useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
-import { getUserById, getWineById, addFavorite, removeFavorite } from "./API";
+import { getUserById, getWineById, addFavorite, removeFavorite, removeSaved, addSaved} from "./API";
 import Rating from "react-rating-stars-component";
 import FollowButton from "./FollowButton";
 
 
 
-const FriendReview = ({reviews, user, favorites}) => {
+const FriendReview = ({reviews, user, favorites, saved}) => {
   const navigate=useNavigate();
   const [reviewFriend, setReviewFriend]= useState({})
   const [wineFriend, setWineFriend]= useState({})
+  const [localFavorites, setLocalFavorites] = useState(favorites || []);
+  const [localSaved, setLocalSaved] = useState(saved || []);
 
-
+  useEffect(() => {
+    setLocalFavorites(favorites || []);
+    setLocalSaved(saved || []);
+  }, [favorites, saved]);
 
   useEffect(()=>{
     async function fetchUser(){
@@ -35,24 +40,61 @@ const formattedPrice = (reviews.price / 100).toLocaleString("en-US", {
 });
 
 function checkOnFaves(wineID) {
-  for (let i = 0; i < favorites.length; i++) {
-    if (favorites[i].wine_id === wineID) {
-      return true;
+  if(localFavorites.length>0){
+    for(const favoriteItem of localFavorites){
+      if(favoriteItem.wine_id===wineID){
+        return favoriteItem.id;
+      }
     }
   }
+  return null;
+  }
+
+function checkOnSaved(wineID) {
+  if (localSaved.length > 0) {
+    for (const savedItem of localSaved) {
+      if (savedItem.wine_id === wineID) {
+        return savedItem.id;
+      }
+    }
+  }
+  return null;
 }
 
-function handleRemoveFavorite(wineID) {
+function handleRemoveFavorite(favoriteID) {
+const updateFavorite=localFavorites.filter(
+  (favoriteItem)=>favoriteItem.id !== favoriteID
+);
+setLocalFavorites(updateFavorite);
+removeFavorite(favoriteID)
+}
 
-  let favoriteId;
-  for (let i = 0; i < favorites.length; i++) {
-    if (favorites[i].wine_id === wineID) {
-      favoriteId = favorites[i].id;
-      break;
-    }
-  }
-  removeFavorite(favoriteId);
-  navigate(`/favorites`);
+function handleRemoveSaved(savedId) {
+  const updatedSaved = localSaved.filter(
+    (savedItem) => savedItem.id !== savedId
+  );
+  setLocalSaved(updatedSaved);
+  removeSaved(savedId);
+}
+
+function handleAddSaved(userId, wineId) {
+  const newSavedItem = {
+    id: Date.now(),
+    user_id: userId,
+    wine_id: wineId,
+  };
+  setLocalSaved([...localSaved, newSavedItem]);
+  addSaved(userId, wineId);
+}
+
+function handleAddFavorite(userId, wineId) {
+  const newFavItem = {
+    id: Date.now(),
+    user_id: userId,
+    wine_id: wineId,
+  };
+  setLocalFavorites([...localFavorites, newFavItem]);
+  addFavorite(userId, wineId);
 }
 
 return (
@@ -65,43 +107,77 @@ return (
           className="img-fluid"
           style={{ maxHeight: "250px", maxWidth: "90%" }}
         />
-        {checkOnFaves(wineFriend.id) ? (
-          <button
-            onClick={() => {
-              handleRemoveFavorite(wineFriend.id);
-            }}
-            className="bg-transparent"
-            style={{
-              position: "absolute",
-              border: "none",
-              top: "1em",
-              right: "1em",
-              zIndex: "0",
-            }}
-          >
-            <span className="material-symbols-outlined">heart_check</span>
-          </button>
-        ) : (
-          <button
-            onClick={() => {
-              addFavorite(user.id, wineFriend.id);
-              navigate(`/favorites`);
-            }}
-            className="bg-transparent"
-            style={{
-              position: "absolute",
-              border: "none",
-              top: "1em",
-              right: "1em",
-              zIndex: "0",
-            }}
-          >
-            <span className="material-symbols-outlined">favorite</span>
-          </button>
-        )}
+      <div className="d-flex flex-column align-items-end" style={{ position: "absolute", top: 0, right: 0 }}>
+        <div
+          onClick={() => {
+            const favoriteId = checkOnFaves(wineFriend.id);
+            if (favoriteId) {
+              handleRemoveFavorite(favoriteId);
+            } else {
+              handleAddFavorite(user.id, wineFriend.id);
+            }
+          }}
+          className="custom-button"
+          style={{
+            marginBottom: "0.5rem",
+            cursor: "pointer",
+          }}
+        >
+          {checkOnFaves(wineFriend.id) ? (
+            <img
+              src="/images/7-heartcheck.png"
+              alt="heart"
+              className="img-fluid"
+              style={{ width: "25%", height: "auto", marginBottom: "-5px", marginRight:"-2px"}}
+              title="Remove from Favorites"
+            />
+          ) : (
+            <img
+              src="/images/5-heart.png"
+              alt="heart"
+              className="img-fluid"
+              style={{ width: "20%", height: "auto" }}
+              title="Add To Favorites"
+            />
+          )}
+        </div>
 
-
+        <div
+          onClick={() => {
+            const savedId = checkOnSaved(wineFriend.id);
+            if (savedId) {
+              handleRemoveSaved(savedId);
+            } else {
+              handleAddSaved(user.id, wineFriend.id);
+            }
+          }}
+          className="custom-button"
+          style={{
+            cursor: "pointer",
+          }}
+        >
+          {checkOnSaved(wineFriend.id) ? (
+            <img
+              src="/images/8-notepad_check.png"
+              alt="notepad"
+              className="img-fluid"
+              style={{ width: "15%", height: "auto", marginTop: "-6px", marginRight:"-2px" }}
+              title="Remove From My List"
+            />
+          ) : (
+            <img
+              src="/images/6-list.png"
+              alt="savedPad"
+              className="img-fluid"
+              style={{ width: "25%", height: "auto" }}
+              title="Add To My List"
+            />
+          )}
+        </div>
       </div>
+      </div>
+
+
      <div className="col-md-9">
       <div className="card-body">
       <h4 className="wine-name">{wineFriend.name}
--- src/components/ReviewDetails.jsx
+++ src/components/ReviewDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getUserById, getWineById, addFavorite, removeFavorite } from "./API";
+import { getUserById, getWineById, addFavorite, removeFavorite, removeSaved, addSaved} from "./API";
 import Rating from "react-rating-stars-component";
 import FollowButton from "./FollowButton";
 
