26a9d3c8fd7a8acfbcc8f98b9d8e9d95b8a3f0c6
Jenniffer Melchiade <jdenisemelch@gmail.com>
2023-08-22 18:12:05 -0400

    working on adjusting profile styling

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -84,132 +84,89 @@ const Profile = ({ user }) => {
   return (
     <div className="profile-container">
       <div id="profile-main" className="d-flex align-items-center flex-wrap">
-        <img
-          src={`/images/${user.avatar}`}
-          alt="avatar image"
-          className="img-fluid"
-          style={{
-            height: "200px",
-            width: "200px",
-            objectFit: "contain",
-            objectPosition: "center center",
-          }}
-        />
-        <div className="ms-4">
-          <h2 className="profile-username">{user.name}</h2>
+        <div className="container-left">
+          <img
+            src={`/images/${user.avatar}`}
+            alt="avatar image"
+            className="img-fluid"
+            style={{
+              height: "200px",
+              width: "200px",
+              objectFit: "contain",
+              objectPosition: "center center",
+            }}
+          />
+          <div className="ms-4">
+            <h2 className="profile-username">{user.name}</h2>
 
-          <div className="d-flex align-items-center">
-            <div className="count-container">
-              <h5 className="profile-username">
-                I follow{" "}
-                <div className="follow-count">
-                  <Link to="/following" className="count-link">
-                    <div
-                      className="count-text"
-                      data-heading={user.following_count}
+            <div className="d-flex align-items-center">
+              <div className="count-container">
+                <h5 className="profile-username">
+                  I follow{" "}
+                  <div className="follow-count">
+                    <Link to="/following" className="count-link">
+                      <div
+                        className="count-text"
+                        data-heading={user.following_count}
+                      >
+                        {user.following_count}
+                      </div>
+                    </Link>
+                  </div>{" "}
+                  people
+                </h5>
+
+                <h5 className="profile-username">
+                  <Link to="/followers" className="count-link">
+                    <span
+                      className="follower-count-text"
+                      data-heading={user.follower_count}
                     >
-                      {user.following_count}
-                    </div>
-                  </Link>
-                </div>{" "}
-                people
-              </h5>
-
-              <h5 className="profile-username">
-                <Link to="/followers" className="count-link">
-                  <span
-                    className="follower-count-text"
-                    data-heading={user.follower_count}
-                  >
-                    {user.follower_count}
-                  </span>
-                </Link>{" "}
-                people follow me!
-              </h5>
+                      {user.follower_count}
+                    </span>
+                  </Link>{" "}
+                  people follow me!
+                </h5>
+              </div>
             </div>
+
+            <p className="thought-bubble">{user.bio}</p>
+          </div>
+        </div>
+        <div className="container-right">
+          <div className="header-container text-center mb-3">
+            <h2 className="profile-username">{user.username}'s Reviews</h2>
+          </div>
+          {user && user.admin ? (
+            <>
+              <Link to={"/users"}>
+                <button type="all users" className="buttons">
+                  All Users
+                </button>
+              </Link>
+            </>
+          ) : null}
+          <div className="row justify-content-center">
+            {userReviews && userReviews.length ? (
+              userReviews.map((userReview) => {
+                return (
+                  <div
+                    key={`userReview-${userReview.id}`}
+                    className="col-md-6 mb-4"
+                  >
+                    <UserReviewDetails
+                      userReviews={userReview}
+                      setUserReviews={setUserReviews}
+                    />
+                  </div>
+                );
+              })
+            ) : (
+              <></>
+            )}
           </div>
         </div>
       </div>
-      <h6 className="profile-username me-3 smaller-text">
-        <span className="me-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="currentColor"
-            className="bi bi-geo-alt"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fillRule="evenodd"
-              d="M6 1a4 4 0 0 1 4 4c0 2.77-2.7 5.935-4 7.745C4.7 11.935 2 8.77 2 5a4 4 0 0 1 4-4zm0 6a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
-            />
-            <path
-              fillRule="evenodd"
-              d="M8 9a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 0 1 0 1H7a.5.5 0 0 1 0-1H8v-2.5A.5.5 0 0 1 8 9z"
-            />
-          </svg>
-        </span>
-        {/* <span className="from-text smaller-text"></span>{" "} */}
-        <span className="user-location smaller-text">{userLocation}</span>
-      </h6>
-      {/* <h6> From: {user.state}</h6> */}
-      <Link to={"/accountsettings"}>
-        <button type="accountsettings" className="btn btn-primary pb-2 mx-2">
-          Account Settings
-        </button>
-      </Link>
-      <p className="thought-bubble">{user.bio}</p>
-      <div className="d-flex flex-wrap pt-3 pb-2">
-        <Link to={"/favorites"}>
-          <button type="favorite" className="btn btn-primary pb-2 mx-2">
-            Favorites
-          </button>
-        </Link>
-        <Link to={"/saved"}>
-          <button type="saved" className="btn btn-primary pb-2 mx-2">
-            Saved
-          </button>
-        </Link>
-      </div>
-
-      <div>
-        <div className="header-container text-center mb-3">
-          <h2 className="profile-username">{user.username}'s Reviews</h2>
-        </div>
-        {user && user.admin ? (
-          <>
-            <Link to={"/users"}>
-              <button type="all users" className="buttons">
-                All Users
-              </button>
-            </Link>
-          </>
-        ) : null}
-      </div>
-      <div className="row justify-content-center">
-        {userReviews && userReviews.length ? (
-          userReviews.map((userReview) => {
-            return (
-              <div
-                key={`userReview-${userReview.id}`}
-                className="col-md-6 mb-4"
-              >
-                <UserReviewDetails
-                  userReviews={userReview}
-                  setUserReviews={setUserReviews}
-                />
-              </div>
-            );
-          })
-        ) : (
-          <></>
-        )}
-      </div>
-      <OptionAvatars
-        followerAvatars={followerAvatars}
-        followingAvatars={followingAvatars}
-      />
     </div>
   );
 };
