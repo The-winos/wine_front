1882d1238be62ade59f4f1c2b470554dfdfbbf9a
Jenniffer Melchiade <jdenisemelch@gmail.com>
2023-09-12 15:43:45 -0400

    added bio to top left container

--- src/components/ProfileOverview.jsx
+++ src/components/ProfileOverview.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ProfileOverview = ({ user }) => {
+  const [expandedBio, setExpandedBio] = useState(false);
   return (
     <div className="profileOverview">
       <div className="profile-overview-container">
         <div className="profile-overview-left-container">
           <div className="top-left container-box">
-            This is the Top-Left Container-Box
+            <div>This is the Top-Left Container-Box</div>
+            {user.bio ? (
+              <div className="user-bio-container">
+                <h6
+                  className={`thought-bubble ${expandedBio ? "expanded" : ""}`}
+                >
+                  {expandedBio ? user.bio : user.bio.substring(0, 150)}
+                  {!expandedBio && (
+                    <span
+                      onClick={() => setExpandedBio(true)}
+                      className="read-more"
+                    >
+                      <small>... (read more)</small>
+                    </span>
+                  )}
+                  {expandedBio && (
+                    <span
+                      onClick={() => setExpandedBio(false)}
+                      className="read-less"
+                    >
+                      <small>(read less)</small>
+                    </span>
+                  )}
+                </h6>
+              </div>
+            ) : (
+              <h5 className="thought-bubble">No bio available.</h5>
+            )}
           </div>
           <div className="bottom-left container-box">
             This is the Bottom-Left Container-Box
