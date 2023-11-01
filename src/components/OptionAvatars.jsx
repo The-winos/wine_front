4917bb4d57b79ef1ef7c85dc0a466c2de555d425
Jessy <japarker0421@gmail.com>
2023-11-01 11:37:35 -0600

    added new profile pictures, formated how they look and update avatar to show avatars bigger as user clicks

--- src/components/OptionAvatars.jsx
+++ src/components/OptionAvatars.jsx
@@ -125,6 +125,383 @@ const OptionAvatars = ({
           cursor: "pointer",
         }}
       />
+      <img
+        src="/images/character10.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character10.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character10.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+      <img
+        src="/images/character11.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character11.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character11.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+      <img
+        src="/images/character12.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character12.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character12.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+        <img
+        src="/images/character13.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character13.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character13.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+        <img
+        src="/images/character14.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character14.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character14.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+        <img
+        src="/images/character15.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character15.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character15.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+        <img
+        src="/images/character16.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character16.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character16.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+        <img
+        src="/images/character17.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character17.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character17.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+        <img
+        src="/images/character18.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character18.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character18.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+        <img
+        src="/images/character19.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character19.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character19.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+        <img
+        src="/images/character20.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character20.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character20.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+        <img
+        src="/images/character21.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character21.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character21.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+        <img
+        src="/images/character22.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character22.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character22.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+        <img
+        src="/images/character23.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character23.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character23.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+        <img
+        src="/images/character24.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character24.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character24.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+        <img
+        src="/images/character25.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character25.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character25.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+        <img
+        src="/images/character26.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character26.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character26.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+        <img
+        src="/images/character27.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character27.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character27.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+        <img
+        src="/images/character28.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character28.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character28.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+      <img
+        src="/images/character29.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character29.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character29.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+      <img
+        src="/images/character30.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character30.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character30.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+      <img
+        src="/images/character31.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character31.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character31.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+      <img
+        src="/images/character32.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character32.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character32.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+      <img
+        src="/images/character33.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character33.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character33.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+      <img
+        src="/images/character34.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character34.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character34.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+      <img
+        src="/images/character35.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character35.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character35.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+      <img
+        src="/images/character36.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character36.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character36.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+      <img
+        src="/images/character37.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character37.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character37.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
+      <img
+        src="/images/character38.png"
+        alt="avatar"
+        className={`avatar ${avatar === "character38.png" ? "selected" : ""}`}
+        onClick={() => setAvatar("character38.png")}
+        style={{
+          height: "50px",
+          width: "50px",
+          objectFit: "contain",
+          objectPosition: "center center",
+          cursor: "pointer",
+        }}
+      />
     </>
   );
 };
