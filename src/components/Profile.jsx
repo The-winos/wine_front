f997405b879a82d286f90df52379cc317a354454
Jessy <japarker0421@gmail.com>
2023-12-14 17:38:25 -0700

    working on getting the most current review

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -110,151 +110,162 @@ const Profile = ({ user }) => {
     <>
       <div className="d-flex flex-wrap">
         <div className="profile-container">
-          {user ? (<>
-          {/* User Information Container */}
-          <div className="user-info-container">
-            <div className="profileAvatar">
+          {user ? (
+            <>
+              {/* User Information Container */}
+              <div className="user-info-container">
+                <div className="profileAvatar">
+                  <img
+                    src={`/images/${user.avatar}`}
+                    alt="avatar image"
+                    className="img-fluid"
+                    style={{
+                      height: "125px",
+                      width: "125px",
+                    }}
+                  />
+                </div>
+                <div className="gear-container">
+                  <img
+                    src="/images/gear1.png"
+                    alt="Gear-1"
+                    className="gear-image"
+                    style={{
+                      height: "85px",
+                      width: "85px",
+                    }}
+                    onClick={() => {
+                      setProfileAccountSettings(true) &
+                        setProfileOverview(false);
+                      setProfileReview(false);
+                      {
+                        handleGearIconClick();
+                      }
+                    }}
+                  />
+                  <div className="gear-text">Account Settings</div>
+                </div>
 
-              <img
-                src={`/images/${user.avatar}`}
-                alt="avatar image"
-                className="img-fluid"
-                style={{
-                  height: "125px",
-                  width: "125px",
-                }}
-              />
-            </div>
-            <div className="gear-container">
-              <img
-                src="/images/gear1.png"
-                alt="Gear-1"
-                className="gear-image"
-                style={{
-                  height: "85px",
-                  width: "85px",
-                }}
-                onClick={() => {
-                  setProfileAccountSettings(true) & setProfileOverview(false);
-                  setProfileReview(false);
-                  {
-                    handleGearIconClick();
-                  }
-                }}
-              />
-              <div className="gear-text">Account Settings</div>
-            </div>
-
-            <div className="d-flex flex-column">
-              <h2 className="profile-username">{user.name}</h2>
-              {/* <div className="user-details"> */}
-              {/* Other user information */}
-              <h6 className="profile-username smaller-text">
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    fill="currentColor"
-                    className="bi bi-geo-alt"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M6 1a4 4 0 0 1 4 4c0 2.77-2.7 5.935-4 7.745C4.7 11.935 2 8.77 2 5a4 4 0 0 1 4-4zm0 6a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
-                    />
-                    <path
-                      fillRule="evenodd"
-                      d="M8 9a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 0 1 0 1H7a.5.5 0 0 1 0-1H8v-2.5A.5.5 0 0 1 8 9z"
-                    />
-                  </svg>
-                </span>
-                <span className="user-location smaller-text">
-                  {userLocation}
-                </span>
-              </h6>
-              <div className="count-container">
-                <h6>
-                  I follow{" "}
-                  <Link to="/following" className="count-link">
-                    <span
-                      className="count-text"
-                      data-heading={user.following_count}
-                    >
-                      {user.following_count}
+                <div className="d-flex flex-column">
+                  <h2 className="profile-username">{user.name}</h2>
+                  {/* <div className="user-details"> */}
+                  {/* Other user information */}
+                  <h6 className="profile-username smaller-text">
+                    <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="currentColor"
+                        className="bi bi-geo-alt"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M6 1a4 4 0 0 1 4 4c0 2.77-2.7 5.935-4 7.745C4.7 11.935 2 8.77 2 5a4 4 0 0 1 4-4zm0 6a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
+                        />
+                        <path
+                          fillRule="evenodd"
+                          d="M8 9a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 0 1 0 1H7a.5.5 0 0 1 0-1H8v-2.5A.5.5 0 0 1 8 9z"
+                        />
+                      </svg>
                     </span>
-                  </Link>
-                  {user.following_count <= 1 ? "person" : "people"}
-                </h6>
-                <h6>
-                  <Link to="/followers" className="count-link">
-                    <span
-                      className="count-text"
-                      data-heading={user.follower_count}
-                    >
-                      {user.follower_count}
+                    <span className="user-location smaller-text">
+                      {userLocation}
                     </span>
-                  </Link>
-                  {user.follower_count <= 1
-                    ? "person follows me"
-                    : "people follow me!"}
-                </h6>
+                  </h6>
+                  <div className="count-container">
+                    <h6>
+                      I follow{" "}
+                      <Link to="/following" className="count-link">
+                        <span
+                          className="count-text"
+                          data-heading={user.following_count}
+                        >
+                          {user.following_count}
+                        </span>
+                      </Link>
+                      {user.following_count <= 1 ? "person" : "people"}
+                    </h6>
+                    <h6>
+                      <Link to="/followers" className="count-link">
+                        <span
+                          className="count-text"
+                          data-heading={user.follower_count}
+                        >
+                          {user.follower_count}
+                        </span>
+                      </Link>
+                      {user.follower_count <= 1
+                        ? "person follows me"
+                        : "people follow me!"}
+                    </h6>
+                  </div>
+                  {/* </div> */}
+                </div>
               </div>
-              {/* </div> */}
-            </div>
-          </div>
 
-          <div className="profileLinks">
-            <div
-              onClick={() => {
-                setProfileOverview(true) &
-                setProfileReview(false) &
-                setProfileFavorites(false) &
-                setProfileAccountSettings(false) &
-                setLinkClicked(true);
-              }}
-            >
-              Overview
-            </div>
-            <div
-              className="profile-review-sidebar"
-              onClick={() => {
-                setProfileReview(true) &
-                  setProfileOverview(false) &
-                  setProfileAccountSettings(false) &
-                  setProfileFavorites(false) &
-                  setLinkClicked(true);
-              }}
-              style={{
-                textDecoration: linkClicked ? "underline" : "none",
-                color: linkClicked ? "#721c24" : "#007bff",
-              }}
-            >
-              Reviews
-            </div>
-            <div
-              className="profile-favorites-sidebar"
-              onClick={() => {
-                setProfileFavorites(true) &
-                  setProfileOverview(false) &
-                  setProfileAccountSettings(false) &
-                  setProfileReview(false) &
-                  setLinkClicked(true);
-              }}
-              style={{
-                textDecoration: linkClicked ? "underline" : "none",
-                color: linkClicked ? "#721c24" : "#007bff",
-              }}
-            >
-              Favorites
-            </div>
-          </div>
-          </>) : (<h5>Loading profile</h5>)}
+              <div className="profileLinks">
+                <div
+                  onClick={() => {
+                    setProfileOverview(true) &
+                      setProfileReview(false) &
+                      setProfileFavorites(false) &
+                      setProfileAccountSettings(false) &
+                      setLinkClicked(true);
+                  }}
+                >
+                  Overview
+                </div>
+                <div
+                  className="profile-review-sidebar"
+                  onClick={() => {
+                    setProfileReview(true) &
+                      setProfileOverview(false) &
+                      setProfileAccountSettings(false) &
+                      setProfileFavorites(false) &
+                      setLinkClicked(true);
+                  }}
+                  style={{
+                    textDecoration: linkClicked ? "underline" : "none",
+                    color: linkClicked ? "#721c24" : "#007bff",
+                  }}
+                >
+                  Reviews
+                </div>
+                <div
+                  className="profile-favorites-sidebar"
+                  onClick={() => {
+                    setProfileFavorites(true) &
+                      setProfileOverview(false) &
+                      setProfileAccountSettings(false) &
+                      setProfileReview(false) &
+                      setLinkClicked(true);
+                  }}
+                  style={{
+                    textDecoration: linkClicked ? "underline" : "none",
+                    color: linkClicked ? "#721c24" : "#007bff",
+                  }}
+                >
+                  Favorites
+                </div>
+              </div>
+            </>
+          ) : (
+            <h5>Loading profile</h5>
+          )}
         </div>
         <div className="profileElements">
           <div>
             {" "}
-            {profileOverview ? <ProfileOverview user={user} currentUser={user}/> : null}
+            {profileOverview ? (
+              <ProfileOverview
+                user={user}
+                currentUser={user}
+                userReviews={userReviews}
+                setUserReviews={setUserReviews}
+              />
+            ) : null}
             {profileReview ? (
               <ProfileReviews
                 user={user}
@@ -272,7 +283,12 @@ const Profile = ({ user }) => {
               />
             ) : null}
           </div>
-          <div> {profileFavorites ? <Favorites user={user} currentUser={user} /> : null}</div>
+          <div>
+            {" "}
+            {profileFavorites ? (
+              <Favorites user={user} currentUser={user} />
+            ) : null}
+          </div>
         </div>
       </div>
     </>
--- src/components/ProfileOverview.jsx
+++ src/components/ProfileOverview.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getReviewByUser, getUserById } from "./API";
+import { getReviewByUser, getUserById, getWineById } from "./API";
 import ProfileReviews from "./ProfileReviews";
 import UserData from "./UserData";
 import { useNavigate, useParams } from "react-router-dom";
@@ -13,14 +13,27 @@ const ProfileOverview = ({
   const [expandedBio, setExpandedBio] = useState(false);
   const navigate = useNavigate();
   const { id } = useParams();
+  const [latestReview, setLatestReview]=useState({})
+  const [latestWine, setLatestWine]=useState({})
 
 
 
   useEffect(() => {
     const fetchUserReviews = async () => {
       try {
-        const reviews = await getReviewByUser(id);
+        if(user){
+        console.log(user.id, "whatcha getting?")
+        console.log(id, "is this hte id?")
+        const reviews = await getReviewByUser(user.id, 'review_date DESC');
         setUserReviews(reviews);
+        setLatestReview(reviews[0])
+        try {
+
+          const newWine= await getWineById(reviews[0].wine_id)
+          setLatestWine(newWine)
+        } catch (error) {
+
+        }}
       } catch (error) {
         console.error(error);
       }
@@ -32,8 +45,9 @@ const ProfileOverview = ({
 
 
   const handleCreateBio = () => {
+    if (user) {
     navigate("/accountsettings");
-  };
+    }}
 
   return (
     <div className="profileOverview">
@@ -91,19 +105,19 @@ const ProfileOverview = ({
           </div>
 
           <div className="bottom-left container-box">
-            <p>
-              Separate user.reviews to render top (stylized) add link to see all
-              (listed)
-            </p>
+
             <p className="reviews-header"></p>
             <div className="profile-overview-reviews">
               <div className="text-center">
-                <h3 className="profile-review-list mx-auto">
-                  {user.username}'s Reviews
-                </h3>
+                <h6 className="profile-review-list mx-auto">
+                  {user.username}'s most current review
+                </h6>
 
                 {userReviews && userReviews.length
-                  ? (console.log(userReviews, "review"))
+                  ? (<>
+                  {latestWine.name}
+{latestReview.name}
+                  </>)
                   : null}
               </div>
               {/* <ProfileReviews user={user} userReviews={{ userReviews }} /> */}
@@ -114,7 +128,7 @@ const ProfileOverview = ({
         <div className="profile-overview-right-container">
           <div className="top-right container-box">
             Insert User Graph/Basic Statistics
-            <UserData userReviews={userReviews} />
+            {/* <UserData userReviews={userReviews} /> */}
           </div>
           <div className="bottom-right container-box">
             Place most popularly rated favorites and saved wine
