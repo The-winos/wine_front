5047eae85c9124e16bc635dd02bd453fd4381efc
Jessy <japarker0421@gmail.com>
2023-12-21 15:09:11 -0700

    fixed profile reviews to match the feeds

--- src/components/ProfileReviews.jsx
+++ src/components/ProfileReviews.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import UserReviewDetails from "./UserReviewDetails";
 import UserData from "./UserData";
 import { Link } from "react-router-dom";
+import ReviewDetails from "./ReviewDetails";
+import { getFavorites, getSaved } from "./API";
 
 const ProfileReviews = ({ user, userReviews, setUserReviews }) => {
   const [selectedNote, setSelectedNote] = useState(null);
   const [seeAllReviewsLink, setSeeAllReviewsLink] = useState(false);
+  const [favorites, setFavorites] = useState([]);
+  const [saved, setSaved] = useState([]);
 
   // Function to handle selecting a note
   const handleNoteClick = (userReview) => {
@@ -19,48 +23,62 @@ const ProfileReviews = ({ user, userReviews, setUserReviews }) => {
 
   // const firstUserReview =
   //   userReviews && userReviews.length ? userReviews[0] : null;
+  useEffect(() => {
+    const fetchUserFavorites = async () => {
+      if (user) {
+        try {
+          const fetchedFavorites = await getFavorites(user.id);
+          setFavorites(fetchedFavorites);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+
+    fetchUserFavorites();
+  }, [user]);
+
+  useEffect(() => {
+    const fetchUserSaved = async () => {
+      if (user) {
+        try {
+          const fetchedSaved = await getSaved(user.id);
+          setSaved(fetchedSaved);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+
+    fetchUserSaved();
+  }, [user]);
 
   return (
     <>
-      <div className="review-card">
+      <div className="profileReviews">
 
-        <div
-          className="header-container text-center mb-3 p-1"
-          onClick={() => {
-            seeAllReviewsLink(true);
-          }}
-        >
-          <Link to="/reviewdetails" className="link-to-reviews">
-            <h3 className="see-reviews-header">See all reviews</h3>
-          </Link>
+        <h2 className="profile-username">{user.username}'s Reviews</h2>
 
-          <h2 className="profile-username">{user.username}'s Reviews</h2>
-        </div>
-        {userReviews && userReviews.length ? (
-          <div className="row pr-0 pl-0">
-            {userReviews.map((userReview) => (
-              <div
-                key={`userReview-${userReview.id}`}
-                className="col-lg-3 col-md-4 col-sm-6 col-12 mb-4"
-              >
-                <div
-                  className={`review-card note ${
-                    selectedNote === userReview.id ? "selected" : ""
-                  }`}
-                  onClick={() => handleNoteClick(userReview)}
-                >
-                  <UserReviewDetails
-                    userReviews={userReview}
-                    setUserReviews={setUserReviews}
+        <div className="review pb-5 mt-5">
+          {userReviews && userReviews.length ? (
+            <div>
+              {userReviews.map((userReview) => (
+                <>
+                  <ReviewDetails
+                    user={user}
+                    review={userReview}
+                    setReviewInfo={setUserReviews}
+                    favorites={favorites}
+                    saved={saved}
                   />
                   <UserData user={user} />
-                </div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div className="text-center">No reviews available.</div>
-        )}
+                </>
+              ))}
+            </div>
+          ) : (
+            <div className="text-center">No reviews available.</div>
+          )}
+        </div>
       </div>
     </>
   );
