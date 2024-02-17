110d832a4fd629450e92b30e9a9a30b0c1525455
Jessy <japarker0421@gmail.com>
2024-02-16 21:56:22 -0700

    working on fixing picutres on deloyed site

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -44,13 +44,13 @@ const Home = ({ allWine, setAllWine, user }) => {
     <div id="Home" className="text-center pt-5 pb-5">
       <div className="home-header">
         <img
-          src="/images/4-wine_glass.png"
+          src="/public/images/4-wine_glass.png"
           alt="Wine Glass"
           className="home-image"
         />
         <h2>C.O.R.K.S</h2>
         <img
-          src="/images/4-wine_glass.png"
+          src="public/images/4-wine_glass.png"
           alt="Wine Glass"
           className="home-image"
         />
